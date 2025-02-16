da650dce3fcb7f027451b1e3ab462570f2c7326e
MarceloCabral98sp <marcelo.cabral98sp@gmail.com>
2025-02-16 14:03:17 -0300

    feat: adding renderer to directives

--- src/app/directives/shadow.directive.ts
+++ src/app/directives/shadow.directive.ts
@@ -1,27 +1,33 @@
-import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
-    selector: '[appShadow]'
+  selector: '[appShadow]',
 })
 export class ShadowDirective implements OnInit {
+  @Input() appShadow: string | number = '5px';
 
-    @Input() appShadow: string | number = '5px';
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit() {
+    this.appShadow = Number(this.appShadow) || 5;
+  }
 
-    ngOnInit() {
-        this.appShadow = Number(this.appShadow) || 5
-    }
+  @HostListener('mouseover') onMouseOver() {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'boxShadow',
+      `0 4px ${this.appShadow}px rgba(0,0,0,0.5)`
+    );
+  }
 
-    @HostListener('mouseover') onMouseOver() {
-        this.renderer.setStyle(
-            this.elementRef.nativeElement,
-            'boxShadow',
-            `0 4px ${this.appShadow}px rgba(0,0,0,0.5)`
-        )
-    }
-
-    @HostListener('mouseleave') onMouseLeave() {
-        this.elementRef.nativeElement.style.boxShadow = 'none';
-    }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'boxShadow', 'none');
+  }
 }
